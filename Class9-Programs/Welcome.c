d1572979e4b292156ab3921c2645c89d9bc217a9
Anushka <312159153+Anushka-Yadav-CSE@users.noreply.github.com>
2026-09-15 22:54:43 +0530

    Added Welcome Program

---
+++ Class9-Programs/Welcome.c
@@ -0,0 +1,10 @@
+// A program to print about C Language.
+#include <stdio.h>
+int main() {
+    // Print information about C language.
+    printf("Introduction to C Programming on VS Code.\n");
+    printf("C Language is developed by Dennis Ritchie.\n");
+    printf("Welcome to C Programming!\n");
+    printf("Enjoy Learning!!!\n");
+    return 0;
+}
